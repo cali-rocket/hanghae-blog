8065b7625817c7fabc128a1dc6c1165cf24932cd
cali-rocket <eastonjubinpark@gmail.com>
2023-05-10 18:26:21 +0900

    lv.2

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 
 const routes = require('./routes');
-/* const logMiddleware = require('./middlewares/logMiddleware'); */
 
 const app = express();
 const port = 3000;
@@ -13,7 +12,6 @@ connect();
 app.use(express.urlencoded({ extended: false })); // form 데이터를 받기 위한 미들웨어
 app.use(express.json()); // json 데이터를 받기 위한 미들웨어
 app.use(cookieParser()); // cookie를 req.cookies에 등록하는 미들웨어
-/* app.use(logMiddleware); */
 
 app.use('/api', routes);
 
